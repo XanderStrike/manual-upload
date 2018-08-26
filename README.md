568886e3eea79b96291613eb213c9ddc37eb329e
Alexander Standke <xanderstrike@gmail.com>
2018-08-25 20:32:52 -0700

    add gratuitous discord integration

--- README.md
+++ README.md
@@ -16,12 +16,14 @@ If you've got it, [Docker](https://www.docker.com/) is the best way to run this 
       -v <path to watchfolder>:/watch \
       -e PGID=1000 -e PUID=1000  \
       -e TZ=America/Los_Angeles \
+      -e DISCORD_URL=<discord webhook url> \
       -p 8080:8080 \
       xanderstrike/manual-upload
 
 * Set the watchfolder to a directory watched by your torrent client
 * PGID and PUID can be found by running `id` in a terminal
-* Timezone is your timezone
+* TZ is your timezone
+* DISCORD_URL is an optional parameter to enable discord notifications of new uploads
 * Configure the port by setting `8080:8080` to `<your port>:8080`
 
 Run with:
--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,46 +1,58 @@
 package controllers
 
 import (
-	"github.com/revel/revel"
-	"fmt"
-	"io/ioutil"
-	"regexp"
+    "github.com/revel/revel"
+    "fmt"
+    "io/ioutil"
+    "regexp"
+    "os"
+    "net/http"
+    "net/url"
 )
 
 const (
-	_      = iota
-	KB int = 1 << (10 * iota)
-	MB
-	GB
+    _      = iota
+    KB int = 1 << (10 * iota)
+    MB
+    GB
 )
 
 type App struct {
-	*revel.Controller
+    *revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+    return c.Render()
 }
 
 func (c App) Upload(torrent []byte, folder string) revel.Result {
-	c.Validation.Required(torrent)
-	c.Validation.MaxSize(torrent, 2*MB).
-		Message("File cannot be larger than 2MB")
+    c.Validation.Required(torrent)
+    c.Validation.MaxSize(torrent, 2*MB).
+        Message("File cannot be larger than 2MB")
 
-	c.Validation.Required(folder)
-	c.Validation.Match(folder, regexp.MustCompile("(movies|music|tv|misc)"))
+    c.Validation.Required(folder)
+    c.Validation.Match(folder, regexp.MustCompile("(movies|music|tv|misc)"))
 
-	if c.Validation.HasErrors() {
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect((*App).Index)
-	}
-
-	err := ioutil.WriteFile(fmt.Sprintf("/watch/%s/%s", folder, c.Params.Files["torrent"][0].Filename), torrent, 0644)
-	if err != nil {
-        panic(err)
+    if c.Validation.HasErrors() {
+        c.Validation.Keep()
+        c.FlashParams()
+        return c.Redirect((*App).Index)
     }
 
-	c.Flash.Success(fmt.Sprintf("Successfully uploaded to %s!", folder))
-	return c.Redirect((*App).Index)
+    filename := c.Params.Files["torrent"][0].Filename
+
+    err := ioutil.WriteFile(fmt.Sprintf("/tmp/%s/%s", folder, filename), torrent, 0644)
+    if err != nil { panic(err) }
+
+    discord_url := os.Getenv("DISCORD_URL")
+
+    if len(discord_url) > 0 {
+    	message := fmt.Sprintf("Uploaded %s to %s folder.", filename, folder)
+        resp, err := http.PostForm(discord_url, url.Values{"content":{message}, "username":{"Manual Upload"}, "avatar_url":{"https://xanderstrike.com/manual.png"}})
+        if err != nil { panic(err) }
+        fmt.Println(resp)
+    }
+
+    c.Flash.Success(fmt.Sprintf("Successfully uploaded to %s!", folder))
+    return c.Redirect((*App).Index)
 }
